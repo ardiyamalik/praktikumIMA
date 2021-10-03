6bf182b189e3c7d55cb5df8b1fb026ebdec0ee61
ardiyamalik <91653831+ardiyamalik@users.noreply.github.com>
2021-10-03 22:07:37 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Ardiya Malik!") ;
+		System.out.println("NIM: 6706210140") ;
+		System.out.println("Kelas: 45-05") ;
+		System.out.println{"IPK:4.0"} ;
+	}
+}
